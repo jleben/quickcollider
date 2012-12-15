4c1ac477c316ba258d48d6f14268b9f23ad288e8
Jakob Leben <jakob.leben@gmail.com>
2012-12-15 15:15:57 +0100

    qml interface: remove unused part of the interface

--- qml_osc_interface.cpp
+++ qml_osc_interface.cpp
@@ -4,26 +4,9 @@
 void QmlOscInterface::componentComplete()
 {
     OscServer::instance()->addInterface(this, mPath);
-
-    /*
-  QObject * target = mTarget;
-  if (!target)
-    target = parent();
-  if (target) {
-    OscServer::instance()->addInterface(target, mPath);
-    connect(target, SIGNAL(destroyed(QObject*)),
-            OscServer::instance(), SLOT(removeInterface(QObject*)));
-  }
-  */
 }
 
 QmlOscInterface::~QmlOscInterface()
 {
     OscServer::instance()->removeInterface(this);
-
-    /*
-  QObject * target = mTarget;
-  if (target)
-    OscServer::instance()->removeInterface(target);
-*/
 }
--- qml_osc_interface.hpp
+++ qml_osc_interface.hpp
@@ -5,13 +5,11 @@
 #include <QQmlParserStatus>
 #include <QByteArray>
 #include <QStringList>
-#include <QPointer>
 
 class QmlOscInterface : public QObject, public QQmlParserStatus
 {
     Q_OBJECT
     Q_INTERFACES(QQmlParserStatus)
-    Q_PROPERTY( QObject* target READ target WRITE setTarget )
     Q_PROPERTY( QString path READ path WRITE setPath )
 
 public:
@@ -20,9 +18,6 @@ public:
     {}
 
     ~QmlOscInterface();
-
-    void setTarget( QObject * object ) { mTarget = object; }
-    QObject *target() { return mTarget; }
 
     void setPath( const QString & path ) { mPath = path.toLatin1(); }
     QString path() const { return QString::fromLatin1(mPath); }
@@ -36,8 +31,6 @@ public:
 private slots:
 
 private:
-
-    QPointer<QObject> mTarget;
     QByteArray mPath;
 };
 
